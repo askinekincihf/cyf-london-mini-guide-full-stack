235938bdcc0f4ccb9ad4796d0dca14838c67426d
askinekinci <askinekincihf@gmail.com>
2021-06-06 12:43:20 +0100

    completed set-up

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="my-3">
+            <h2>City Mini Guide</h2>
+        </div>
+    )
+}
+
+export default Header
